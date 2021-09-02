ad02d6436cfd1a44ee3ea976e7138cc002fe9671
Simon <simonorugbo15@gmail.com>
2021-09-02 10:38:36 +0100

    added preview collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
